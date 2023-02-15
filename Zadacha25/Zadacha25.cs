31cbb7bf413d6fc642b8612530cfe1f842932325
AlexIv <ivlieva.medphyslab.com>
2023-02-15 22:44:07 +0300

    Body of function is done. Function works

--- Zadacha25/Zadacha25.cs
+++ Zadacha25/Zadacha25.cs
@@ -3,7 +3,10 @@
 3, 5 -> 243 (3⁵)
 
 2, 4 -> 16*/
-
+System.Console.WriteLine("Enter A: ");
+double A=double.Parse(Console.ReadLine());
+System.Console.WriteLine("Enter B: ");
+int B=int.Parse(Console.ReadLine());
 double AtodegreeB (double A, int B)
 {
     if (B<=0)
@@ -12,6 +15,12 @@ double AtodegreeB (double A, int B)
         return 0;
     }
     else {
-        
+        double AinB=A;
+        for (int index=1; index<B; index++)
+        {
+            AinB=AinB*A;
+        }
+        return AinB;
     }
 }
+System.Console.WriteLine(AtodegreeB (A,B));
