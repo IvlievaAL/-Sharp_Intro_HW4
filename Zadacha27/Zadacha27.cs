20fdb9e6abc1f0ab8c85b943b2c5441565dd274f
AlexIv <ivlieva.medphyslab.com>
2023-02-15 23:01:48 +0300

    Base from stock added

--- Zadacha27/Zadacha27.cs
+++ Zadacha27/Zadacha27.cs
@@ -7,3 +7,31 @@
 9012 -> 12*/
 System.Console.WriteLine("Enter N: ");
 int N=int.Parse(Console.ReadLine());
+int NumberofDigits(int number)
+ {
+if (number==0)
+{
+  return 1;
+}
+else
+{
+int absnumber=Math.Abs(number);
+int Ndigits=1;
+int Ndivided=(absnumber/10);
+for (; Ndivided>=1;Ndigits++)
+   {
+      Ndivided=Ndivided/10;
+   }
+int[] arrayN=new int[Ndigits];
+Ndivided=Ndivided+(absnumber/10);
+int Ndivided1=absnumber%10;
+for (int index=Ndigits-1;index>=0;index=index-1)
+   {
+      arrayN[index]=Ndivided1;
+      Ndivided1=Ndivided%10;
+      Ndivided=Ndivided/10;
+   }
+ return arrayN[Ndigits-1];
+}
+  }
+System.Console.WriteLine(NumberofDigits(number));
