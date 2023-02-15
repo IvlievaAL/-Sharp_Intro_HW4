a7ae92910cfc1dff9f2969b600645e7860b40b14
AlexIv <ivlieva.medphyslab.com>
2023-02-15 23:45:17 +0300

    Code draft is written

--- Zadacha27/Zadacha27.cs
+++ Zadacha27/Zadacha27.cs
@@ -7,22 +7,21 @@
 9012 -> 12*/
 System.Console.WriteLine("Enter N: ");
 int N=int.Parse(Console.ReadLine());
-int NumberofDigits(int number)
- {
-if (number==0)
-{
-  return 1;
-}
-else
-{
-int absnumber=Math.Abs(number);
 int Ndigits=1;
+int absnumber=Math.Abs(N);
 int Ndivided=(absnumber/10);
+int CountingDigitsinNumber (int N) //подзадача - посчитать количество цифр в числе
+  {
 for (; Ndivided>=1;Ndigits++)
    {
       Ndivided=Ndivided/10;
    }
-int[] arrayN=new int[Ndigits];
+  return Ndigits;
+  }
+int [] NumbertoMassive (int N) //подзадача - положить цифры числа в массив
+  {
+   CountingDigitsinNumber(N);
+   int[] arrayN=new int[Ndigits];
 Ndivided=Ndivided+(absnumber/10);
 int Ndivided1=absnumber%10;
 for (int index=Ndigits-1;index>=0;index=index-1)
@@ -31,7 +30,23 @@ for (int index=Ndigits-1;index>=0;index=index-1)
       Ndivided1=Ndivided%10;
       Ndivided=Ndivided/10;
    }
- return arrayN[Ndigits-1];
-}
+ return arrayN;
   }
-System.Console.WriteLine(NumberofDigits(number));
+int SumofDigits(int N) //сама функция подсчета суммы цифр числа
+ {
+if (N==0)
+{
+  return 0;
+}
+else
+{
+ NumbertoMassive (N);
+ int Sum=0;
+ for (int index=0;index>N;index++)
+   {
+    Sum=Sum+NumbertoMassive (N)[index];
+   }
+   return Sum;
+ }
+  }
+System.Console.WriteLine(SumofDigits(N));
