d7d4ab672e53e1343f6868c043f732d89e7d7423
AlexIv <ivlieva.medphyslab.com>
2023-02-16 21:07:14 +0300

    massive is filled with 8 random numbers

--- Zadacha29/Zadacha29.cs
+++ Zadacha29/Zadacha29.cs
@@ -3,3 +3,15 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 
 6, 1, 33 -> [6, 1, 33]*/
+
+void PrintMassiveOf8RandomNumbers()
+{
+ int [] ArrayOf8Numbers= new int [8];
+ Random rand=new Random();
+ for (int index=0; index<8; index++)
+ {
+    ArrayOf8Numbers[index]=rand.Next();
+    System.Console.WriteLine (ArrayOf8Numbers[index]);
+ }
+}
+PrintMassiveOf8RandomNumbers();
